b6082dff6519f5edc77f48b2c92e356b255bab5d
ggarushi <garushiarora1999@gmail.com>
2021-03-17 01:24:49 +0530

    center aligned text

--- lib/SIgninfiles/main.dart
+++ lib/SIgninfiles/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'Homepage.dart';
@@ -22,36 +23,42 @@ class Myhome extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.teal,
         body :SafeArea(
-            child:Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:<Widget>[
-            Text("Let's manage together..",
-            style:TextStyle(
-              fontWeight: FontWeight.bold,
-              fontStyle: FontStyle.italic,
-              fontSize: 40,
-              color:Colors.white,
-            )),
-               SizedBox(height:20),
-               RaisedButton(
-                  child: Text('Start'),
-                 padding: const EdgeInsets.all(15.0),
-                 textColor: Colors.black,
-                 color: Colors.white,
-                  hoverElevation: 20.0,
-                  onPressed: () {
-                    print('Pressed');
-//
-                    Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Homepage()),
-                    );
-                  },
-                )
-              ],
+            child:Align(
+              alignment: Alignment.center,
+              child: Column(
+//              crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:<Widget>[
+              Text("Let's manage together...",
+                  textAlign:TextAlign.center,
+              style:TextStyle(
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                fontSize: 40,
+                color:Colors.white,
 
-        )
+
+              )),
+                 SizedBox(height:20),
+                 RaisedButton(
+                    child: Text('Start'),
+                  padding: const EdgeInsets.all(15.0),
+                   textColor: Colors.black,
+                   color: Colors.white,
+                    hoverElevation: 20.0,
+                    onPressed: () {
+                      print('Pressed');
+//
+                      Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Homepage()),
+                      );
+                    },
+                  )
+                ],
+
+        ),
+            )
       ) ,
     );
   }
